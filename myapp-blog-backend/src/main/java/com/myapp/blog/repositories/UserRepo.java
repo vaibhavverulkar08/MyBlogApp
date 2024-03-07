bfa35a89e1b0e1d0424c31f2003cbb665f8d4deb
vaibhavverulkar08 <vaibhavverulkar08@gmail.com>
2024-03-07 17:07:10 +0530

    Remove myapp-blog-backend folder

--- myapp-blog-backend/src/main/java/com/myapp/blog/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.myapp.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.myapp.blog.entities.User;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-
-}
