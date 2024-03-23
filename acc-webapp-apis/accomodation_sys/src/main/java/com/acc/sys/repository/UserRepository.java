90c9f79dc9f615e8da9af43d3ec941aa0eae29c9
vaibhavverulkar08 <vaibhavverulkar08@gmail.com>
2024-03-23 12:31:57 +0530

    Remove acc-webapp-apis directory

--- acc-webapp-apis/accomodation_sys/src/main/java/com/acc/sys/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acc.sys.repository;
-import com.acc.sys.model.Model;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserRepository extends CrudRepository<Model,Integer>{
-	public Model findByEmail(String email);
-
-}
